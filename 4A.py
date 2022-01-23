f7165e62524cbb7dc8dd2d5c900946ab7f327974
mamon_saleh <mamonsaleh2@gmail.com>
2022-01-23 02:21:07 +0200

    update files

---
+++ 4A.py
@@ -0,0 +1,6 @@
+### problem 4A
+##################################
+#Input the weight of the watermelon
+a=int(input())
+#check if the weight can be divided into two even numbers
+print("Yes") if a%2==0 and a!=2 else print("No")
--- A44.py
+++
@@ -1,5 +0,0 @@
-### this is the first problem for this month 
-a=1
-b=2
-c=3
-print("Hello World")
